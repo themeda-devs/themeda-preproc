e77790742fc28b16f6983a7865cdb27f573250e3
Damien Mannion <damien.mannion@unimelb.edu.au>
2023-08-24 09:46:20 +1000

    -

--- src/ecofuture_preproc/roi.py
+++ src/ecofuture_preproc/roi.py
@@ -1,10 +1,17 @@
+# allows methods in the class to have variables of the same type as the class
+from __future__ import annotations
+
 import enum
 import pathlib
 import typing
-import dataclasses
 import importlib.resources
+import pickle
+
+import numpy as np
+import numpy.typing as npt
 
 import shapely
+import pyproj
 
 
 class ROIName(enum.Enum):
@@ -15,42 +22,116 @@ class ROIName(enum.Enum):
         return self.value
 
 
-@dataclasses.dataclass
 class RegionOfInterest:
-    roi_name: ROIName
 
-    # TODO
+    __slots__ = ("name", "_shape", "base_output_dir")
+
+    _geojson_filename_lut : typing.ClassVar = {
+        "savanna": "NAust_mask_RoI_WGS1984.geojson",
+        "australia": "Aust_coastline_mainland_WGS1984.geojson",
+    }
+
+    def __init__(
+        self,
+        name: ROIName,
+        base_output_dir: typing.Optional[pathlib.Path] = None,
+        load: bool = True,
+    ) -> None:
+        self.name = name
+
+        self.base_output_dir = base_output_dir
+
+        if load:
+            self.load()
+        else:
+            self._shape = None
 
     @property
-    def geojson_path(self):
+    def shape(self) -> shapely.geometry.polygon.Polygon:
+        if self._shape is None:
+            raise ValueError("Need to run `.load()` first")
+        return self._shape
 
-        geojson_filename_lut = {
-            "savanna": "NAust_mask_RoI_WGS1984.geojson",
-            "australia": "Aust_coastline_mainland_WGS1984.geojson",
-        }
+    @property
+    def shape_path(self) -> pathlib.Path:
+        if self.base_output_dir is None:
+            raise ValueError("Need to set `base_output_dir`")
+        return (
+            self.base_output_dir
+            / "roi"
+            / f"ecofuture_roi_shape_{self.name.value}.pkl"
+        )
 
-        path = importlib.resources.files("ecofuture_preproc.resources.roi").joinpath(
-            geojson_filename_lut[self.roi_name.value]
+    @property
+    def geojson_path(self) -> pathlib.Path:
+
+        path = pathlib.Path(
+            str(
+                importlib.resources.files("ecofuture_preproc.resources.roi").joinpath(
+                    self._geojson_filename_lut[self.name.value]
+                )
+            )
         )
 
         return path
 
+    def intersects_with(self, other_roi: RegionOfInterest) -> bool:
+        "'Returns True if A and B share any portion of space'"
+        intersects: bool = self.shape.intersects(other=other_roi.shape)
+        return intersects
 
+    def is_within(self, other_roi: RegionOfInterest) -> bool:
+        "'Returns True if geometry A is completely inside geometry B'"
+        within: bool = self.shape.within(other=other_roi.shape)
+        return within
 
-def run_prep(
-    roi_name: ROIName,
-    base_output_dir: typing.Union[pathlib.Path, str],
-    protect: bool,
-    show_progress: bool,
-) -> None:
+    def prepare(self, src_crs: int = 4326, dst_crs: int = 3577) -> None:
+        """
+        Loads a spatial region-of-interest from a GeoJSON file, optionally converts the
+        projection, and formats the region as a `shapely` Polygon.
+        """
 
-    base_output_dir = pathlib.Path(base_output_dir)
+        geoms = shapely.from_geojson(geometry=self.geojson_path.read_text())
 
+        try:
+            (region,) = geoms.geoms
+        except ValueError as err:
+            raise ValueError(
+                "Unexpected number of components in the region file"
+            ) from err
 
-def get_roi_geojson_path(
-    roi_name: typing.Union[ROIName, str],
-) -> pathlib.Path:
+        if src_crs != dst_crs:
+            transformer = pyproj.Transformer.from_crs(
+                crs_from=src_crs,
+                crs_to=dst_crs,
+                always_xy=True,
+            )
 
-    if isinstance(roi_name, str):
-        roi_name = ROIName(roi_name)
+            def shapely_transform(points: npt.NDArray[float]) -> npt.NDArray[float]:
+                return np.column_stack(
+                    transformer.transform(
+                        xx=points[:, 0],
+                        yy=points[:, 1],
+                        errcheck=True,
+                    )
+                )
 
+            region = shapely.transform(
+                geometry=region,
+                transformation=shapely_transform,
+            )
+
+        # might speed up calls that use the geometry
+        shapely.prepare(geometry=region)
+
+        self.shape_path.parent.mkdir(parents=True, exist_ok=True)
+
+        with self.shape_path.open("wb") as handle:
+            pickle.dump(region, handle)
+
+        self.load()
+
+    def load(self) -> None:
+
+        with self.shape_path.open("rb") as handle:
+            self._shape = pickle.load(handle)
