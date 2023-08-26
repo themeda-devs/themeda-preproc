38be21c9ff40544ade543384ef15b1b64a3b4139
Damien Mannion <damien.mannion@unimelb.edu.au>
2023-08-26 16:35:15 +1000

    -

--- run.sh
+++ run.sh
@@ -6,3 +6,7 @@ poetry run ecofuture_preproc roi_prep -roi_name australia
 
 # DEA land cover
 poetry run ecofuture_preproc acquire -source_name land_cover
+
+# ANU climate
+poetry run ecofuture_preproc acquire -source_name rain
+poetry run ecofuture_preproc acquire -source_name tmax
